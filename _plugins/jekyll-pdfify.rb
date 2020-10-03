2128fdce149947105928fb06a02d0fbc204ac34d
Robert Randolph <audiolabs@gmail.com>
2020-10-03 17:08:01 -0400

    pdf generation stuff.

--- _plugins/jekyll-pdfify.rb
+++ _plugins/jekyll-pdfify.rb
@@ -1,18 +1,31 @@
 require 'fileutils'
+require 'open3'
 
-Jekyll::Hooks.register :site, :post_read do |site|
-  site.posts.each { |post| 
-    new_post = Marshal.load(Marshal.dump(post))
-    p new_post
-  }
-    
-  # site.posts << new_post }
+# Generates a list of files (with pdf: true and a reader layout) to be converted.
+# ./generate-pdfs starts a server and uses wkhtmltopdf to convert the listed files.
 
-  # filename = "_site/reader" + post.url
-
-  # dirname = File.dirname(filename)
-  # unless File.directory?(dirname)
-  #   FileUtils.mkdir_p(dirname)
-  # end
-  # File.open(filename, "w") { |f| f.write post }
+Jekyll::Hooks.register :posts, :post_write do |post|
+  if post.data["pdf"]
+    if post.data["layout"] == "reader"
+      filename = "_site" + post.url
+      dirname = File.dirname(filename)
+      unless File.directory?(dirname)
+        FileUtils.mkdir_p(dirname)
+      end
+      open("_site/pdf.log", "a") do |f|
+        f.puts filename.gsub(/\_site\//, "")
+      end
+      # --- For when there's a working PDF command ---
+      # open(filename, 'w') do |f|
+      #     pdf_name = File.dirname(filename) + "/" + File.basename(filename, ".*") + ".pdf"
+      #     pdf_name = pdf_name.gsub(/\_site/, "")
+      #     # paths starting with / are absolute. Must have separate PDF script that starts server
+      #     # and generates from a list of paths
+      #     out, err, st = Open3.capture3("wkhtmltopdf", "--enable-local-file-access", filename, pdf_name)
+      #     open("_site/pdf.log", "a") do |f|
+      #       f.puts filename
+      #     end
+      # end
+    end
+  end
 end
